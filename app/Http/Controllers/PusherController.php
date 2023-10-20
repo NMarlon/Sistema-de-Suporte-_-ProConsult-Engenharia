df18b630e6e24c9d015f0b9faa80caf012936c3e
Marlon Rebello Viana <marlonrviana@gmail.com>
2023-10-20 10:02:45 -0300

    Chat funcional! (básico do básico)

--- app/Http/Controllers/PusherController.php
+++ app/Http/Controllers/PusherController.php
@@ -2,22 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Events\PusherBroadcast;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
 
 class PusherController extends Controller
 {
-    //
-    public function index(){
-        return view ( view:'index');
+    public function index()
+    {
+        return view('index');
     }
 
-    public function broadcast(Request $request){
-        broadcast(new PusherBroadcast($request->get(key:'message')))->toOthers();
-        return view(view: 'broadcast', ['message' => $request->get(key:'message')]);
+    public function broadcast(Request $request)
+    {
+        broadcast(new PusherBroadcast($request->get('message')))->toOthers();
+
+        return view('broadcast', ['message' => $request->get('message')]);
     }
 
-    public function receive(Request $request){
-          
+    public function receive(Request $request)
+    {
+        return view('receive', ['message' => $request->get('message')]);
     }
 }
